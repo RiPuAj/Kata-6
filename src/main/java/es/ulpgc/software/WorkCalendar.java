9d2134244ac18503207861d8de3338d7cd970bb3
RiPuAj <javipulido15@gmail.com>
2025-01-17 06:27:29 +0000

    Implemented WorkCalendar

---
+++ src/main/java/es/ulpgc/software/WorkCalendar.java
@@ -0,0 +1,36 @@
+package es.ulpgc.software;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Iterator;
+import java.util.Set;
+
+public class WorkCalendar {
+    private final Set<DayOfWeek> workingDays = Set.of(
+            DayOfWeek.MONDAY,
+            DayOfWeek.TUESDAY,
+            DayOfWeek.WEDNESDAY,
+            DayOfWeek.THURSDAY,
+            DayOfWeek.FRIDAY
+    );
+
+    public Iterator<LocalDate> iteratorFor(LocalDate date) {
+        return new Iterator<LocalDate>() {
+            private LocalDate current = date;
+
+            public boolean hasNext() {
+                return true;
+            }
+
+            public LocalDate next() {
+                var next = current.plusDays(1);
+                while(!isWorkingDay(next)) next = current.plusDays(1);
+                return next;
+            }
+        };
+    }
+
+    private boolean isWorkingDay(LocalDate next) {
+        return workingDays.contains(next.getDayOfWeek());
+    }
+}
